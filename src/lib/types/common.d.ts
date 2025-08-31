acc9f528eb308798fb587067da0f388800cf085f
Ziad Mahmoud <ziaddmahmoude@gmail.com>
2025-08-31 22:35:26 +0300

    firts commit

--- src/lib/types/common.d.ts
+++ src/lib/types/common.d.ts
@@ -1,14 +1,3 @@
-declare type SearchParams = string | string[] | undefined;
-
-declare type RouteProps = {
-  params: promise<{ locale: Locale; productSlug: string }>;
-  searchParams: SearchParams;
-};
-
-declare type LayoutProps = {
-  children: React.ReactNode;
-} & Pick<RouteProps, "params">;
-
 declare type mainData = {
   uuid: string;
   name: string;
