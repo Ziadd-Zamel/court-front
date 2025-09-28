2a27cc8f82fc39a7aa265fa962524ecec747eeb6
Ziadd-Zamel <ziaddmahmoude@gmail.com>
2025-09-28 22:28:12 +0300

    new changes

--- src/app/(homepage)/article/[articleId]/_components/page-content.tsx
+++ src/app/(homepage)/article/[articleId]/_components/page-content.tsx
@@ -167,7 +167,7 @@ export default function PageContent({ article }: { article: Article }) {
           tabs={contentTabs}
           defaultValue={contentTabs[0]?.value || ""}
           className="w-full flex justify-center items-center mt-12 "
-          tabListClassName="mb-10 max-w-none flex-wrap"
+          tabListClassName="mb-7 max-w-none flex-wrap"
         />
       </section>
     </>
